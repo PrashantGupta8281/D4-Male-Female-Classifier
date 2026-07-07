375e776ea9e8d710e9d773958375a5f7eb3768ee
PrashantGupta8281 <guptaprashant2006@gmail.com>
2026-07-07 23:44:41 +0530

    Update app (2).py

--- app (2).py
+++ app (2).py
@@ -15,7 +15,7 @@ st.set_page_config(
 # -------------------------
 # Load Model
 # -------------------------
-model = joblib.load("Male_Female_model.pkl")
+model = joblib.load("Male_And_Female_model.pkl")
 
 IMG_SIZE = 64
 
